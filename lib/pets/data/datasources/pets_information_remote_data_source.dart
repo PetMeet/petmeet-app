e307271d0d95a9e100f2c8e6031e04b1f74c2fc9
anjovazquez <anjo.vazquez@gmail.com>
2019-11-17 12:14:49 +0100

    pets information datasource

--- lib/pets/data/datasources/pets_information_remote_data_source.dart
+++ lib/pets/data/datasources/pets_information_remote_data_source.dart
@@ -1,7 +1,13 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:pet_team/pets/data/models/pet_information_model.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
+import 'dart:async';
 
 import 'package:http/http.dart' as http;
+
+import '../models/pet_information_model.dart';
+import '../models/pet_information_model.dart';
 
 abstract class PetsInformationRemoteDataSource {
     Future<List<PetInformationModel>> getPetsInformation();
@@ -14,7 +20,10 @@ class PetsInformationRemoteDataSourceImpl implements PetsInformationRemoteDataSo
   PetsInformationRemoteDataSourceImpl({@required this.client});
 
   @override
-  Future<List<PetInformationModel>> getPetsInformation() {
-    return null;
+  Future<List<PetInformationModel>> getPetsInformation() async {
+    List<PetInformationModel> petList = new List<PetInformationModel>();
+    List<dynamic> petsJson = json.decode(await rootBundle.loadString(""));
+    petList = petsJson.map((i)=>PetInformationModel.fromJson(i)).toList();
+    return petList; 
   }
 }
