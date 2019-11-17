4ebbc10f169fd28c4799d20928fafd0bcbc48d5a
angel.vazquez <angel.vazquez@imatia.com>
2019-11-17 17:54:37 +0100

    added json mock data

--- lib/pets/data/datasources/pets_information_local_data_source.dart
+++ lib/pets/data/datasources/pets_information_local_data_source.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:pet_team/pets/data/models/pet_information_model.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
+import 'dart:async';
+
 
 abstract class PetsInformationLocalDataSource {
     Future<List<PetInformationModel>> getPetsInformation();
@@ -9,7 +14,10 @@ class PetsInformationLocalDataSourceImpl implements PetsInformationLocalDataSour
   PetsInformationLocalDataSourceImpl();
 
   @override
-  Future<List<PetInformationModel>> getPetsInformation() {
-    return null;
+  Future<List<PetInformationModel>> getPetsInformation() async {
+    List<PetInformationModel> petList = new List<PetInformationModel>();
+    List<dynamic> petsJson = json.decode(await rootBundle.loadString("assets/jsons/pets_information.json"));
+    petList = petsJson.map((i)=>PetInformationModel.fromJson(i)).toList();
+    return petList; 
   }
 }
